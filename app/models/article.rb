d29c4e047479ef3b0568226af0fed4db42e7bf99
derrick-ho <derrickjho@protonmail.com>
2019-02-28 12:20:09 -0800

    Removes orphaned taggings?

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
    has_many :comments
    has_many :taggings
-   has_many :tags, through: :taggings
+   has_many :tags, through: :taggings, dependent: :destroy
 
    has_attached_file :image
    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
